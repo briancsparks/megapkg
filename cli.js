0978b1d5ba94a7f8bc948322d6aab1f862430499
Brian C Sparks <briancsparks@gmail.com>
2021-12-25 04:49:44 -0800

    some files

---
+++ cli.js
@@ -0,0 +1,37 @@
+
+
+
+
+
+function load(existing ={}) {
+  let result = {};
+
+  result = {
+    ...result,
+    mkdirp: require('mkdirp')
+  };
+
+  return result;
+}
+
+/*
+ * dotenv,loud-rejection,exit-on-epipe
+ * underscore,lodash,ramda
+ * commander,inquirer,prompts,enquirer
+ * ncp,mkdirp,shelljs,fs-extra,detect-file,fs-exists-sync,execa,glob
+ * string-width,boxen,log-update,cli-table,split,split2
+ * micromatch,axios
+ * terminal-kit,gray-matter,detect-file,command-exists,locate-path,path-exists
+ * prompts,luxon
+ * yargs,optimist,minimist
+ * ora,ansi-styles,chalk,cli-highlight,cli-spinners,progress,listr,widest-line,chalk
+ *
+ * deep-equal,deep-extend
+ *
+ * os,path,fs,readline,child-process
+ *
+ * ink
+ *
+ *
+ *
+ */
